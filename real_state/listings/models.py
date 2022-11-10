1fe4df146a35e97d7e3417cd37670f47318382e6
Daniel Alves <danielribeiroalves0@gmail.com>
2022-11-10 13:55:47 -0300

    Creating the database migrations.

--- real_state/listings/models.py
+++ real_state/listings/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# Models are basically the representation of the database schema. For example, on table in the database will be converter into a python object.
+# This allow to iterate to the database with python. 
+
+
+# The "Model" allows the class to be interpreted as a database table.
+class Listing(models.Model):
+    title = models.CharField(max_length=150)
+    price = models.IntegerField()
+    num_bedrooms = models.IntegerField()
+    num_bathrooms = models.IntegerField()
+    square_footage = models.IntegerField()
+    address =  models.CharField(max_length=100)
+#   image = models.ImageField()
