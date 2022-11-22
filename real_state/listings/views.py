a9760e568eab72cc724e760e4616e3ca7d3b751f
Daniel Alves <danielribeiroalves0@gmail.com>
2022-11-21 22:05:26 -0300

    Creating listing function.

--- real_state/listings/views.py
+++ real_state/listings/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Listing
 
-# Create your views here.
+
+# Using functions for simpler understanding 
+def listing_list(request):
+    # Return all rows in the table
+    listings = Listing.objects.all()
+
+    # Defining a dictionary
+    context = {
+        "listings": listings
+    }
+    
+    # To return a response in Django is common to use the render
+    return render(request, "listings.html", context)
