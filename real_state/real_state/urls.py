fa753f80fc15acbf97266ac5e1601a6e95a6f493
Daniel Alves <danielribeiroalves0@gmail.com>
2022-11-21 22:21:01 -0300

    Adding new endpoint.

--- real_state/real_state/urls.py
+++ real_state/real_state/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path
 
 from listings.views import listing_list
+from listings.views import listing_retrieve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', listing_list)
+    path('', listing_list),
+    path('listings/<pk>/', listing_retrieve)
 ]
