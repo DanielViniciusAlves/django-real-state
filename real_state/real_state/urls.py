a20accb93dd4d764debc5b6be1a6085cd8ae07e9
Daniel Alves <danielribeiroalves0@gmail.com>
2022-11-23 21:17:43 -0300

    Adding endpoints for the new CRUD functions.

--- real_state/real_state/urls.py
+++ real_state/real_state/urls.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
 from django.urls import path
 
-from listings.views import listing_list
-from listings.views import listing_retrieve
-from listings.views import listing_create
+from listings.views import (
+    listing_list,
+    listing_create,
+    listing_retrieve,
+    listing_update,
+    listing_delete
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', listing_list),
     path('listings/<pk>/', listing_retrieve),
-    path('add-listing/', listing_create)
+    path('add-listing/', listing_create),
+    path('listings/<pk>/edit/', listing_update),
+    path('listings/<pk>/delete/', listing_delete)
 ]
